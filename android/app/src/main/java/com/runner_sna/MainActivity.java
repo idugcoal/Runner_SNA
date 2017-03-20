0fff58fc3fb167f99ee7e79952fe6e387d071630
Doug Cole <idugcoal@gmail.com>
2017-03-20 12:21:32 -0700

    first commit

---
+++ android/app/src/main/java/com/runner_sna/MainActivity.java
@@ -0,0 +1,15 @@
+package com.runner_sna;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Runner_SNA";
+    }
+}

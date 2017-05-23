756dda70f145d3836876492a899d4d76042754ac
Doug Cole <idugcoal@gmail.com>
2017-05-22 20:35:30 -0700

    changes # of gates from 15 to 21

--- src/components/SelectGate.js
+++ src/components/SelectGate.js
@@ -10,7 +10,9 @@ import Style from './Style';
 const gates = [
   [1, 2, 3, 4, 5],
   [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15]
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22]
 ];
 
 class SelectGate extends Component {
